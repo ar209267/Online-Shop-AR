a1ccf933d91d3064be68f7e5ac8ae87af521ef13
ar209267 <rvai80054@gmail.com>
2026-05-10 20:08:40 +0600

    Add database configuration in config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db_name = 'আপনার_ডেটাবেস_নাম';
+$db_user = 'আপনার_ইউজার_নাম';
+$db_pass = 'আপনার_পাসওয়ার্ড';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
